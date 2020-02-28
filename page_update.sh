a70d79174615c4889a7363dd9dbf0c54b71eb1a0
aniket2797 <53761347+aniket2797@users.noreply.github.com>
2020-02-28 16:39:34 +0530

    Update page_update.sh

--- page_update.sh
+++ page_update.sh
@@ -3,28 +3,28 @@
 DATE=$(date)
 
 # check default page of new hosting
-if diff /SCRIPTS/UPDATEME/default_html.html /usr/local/CyberCP/index.html > /dev/null ; then
+if diff /W3SERVICES_SCRIPT/UPDATEME/default_html.html /usr/local/CyberCP/index.html > /dev/null ; then
 {
 echo "Same File"
 }
 else
 {
-cp /SCRIPTS/UPDATEME/default_html.html /usr/local/CyberCP/index.html
+cp /W3SERVICES_SCRIPT/UPDATEME/default_html.html /usr/local/CyberCP/index.html
 echo "Default File updated"
 }
 fi
 
 
 # check default page of login page
-if diff /SCRIPTS/UPDATEME/login.html /usr/local/CyberCP/loginSystem/templates/loginSystem/login.html > /dev/null ; then
+if diff /W3SERVICES_SCRIPT/UPDATEME/login.html /usr/local/CyberCP/loginSystem/templates/loginSystem/login.html > /dev/null ; then
 {
 echo "Same File"
 }
 else
 {
-cp /SCRIPTS/UPDATEME/login.html /usr/local/CyberCP/loginSystem/templates/loginSystem/login.html
+cp /W3SERVICES_SCRIPT/UPDATEME/login.html /usr/local/CyberCP/loginSystem/templates/loginSystem/login.html
 echo "Login File updated"
 }
 fi
 
-echo "Last Check done at $DATE" >> /SCRIPTS/UPDATEME/log/page_update.log
+echo "Last Check done at $DATE" >> /W3SERVICES_SCRIPT/UPDATEME/log/page_update.log
